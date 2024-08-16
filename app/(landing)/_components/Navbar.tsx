dc1c8e197e6df9b09686cc31bb70497b1cfaeaf7
shradha mantry <itsmehak27@gmail.com>
2024-08-16 19:18:07 +0530

    landingPage update

--- app/(landing)/_components/Navbar.tsx
+++ app/(landing)/_components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="w-full fixed max-w-xl top-0 md:top-4 left-1/2 -translate-x-1/2 flex items-center gap-4 p-8 py-3 md:rounded-full text-davy bg-white z-50">
+    <div className="w-full fixed max-w-xl top-0 md:top-4 left-1/2 -translate-x-1/2 flex items-center gap-4 p-8 py-3 md:rounded-full text-white  bg-[rgba(12,26,60,0.8)] z-50">
       {/* Logo */}
       <Link href={"/"} className="flex items-center gap-2 mr-auto">
         <Image
@@ -19,12 +19,12 @@ export default function Navbar() {
         </h2>
       </Link>
 
-      {/*  */}
+      {/* Navigation Links */}
       <Link href={"/sign-up"}>
-        <Button variant={"link"}>Sign Up</Button>
+        <Button variant={"link"} className="text-white text-md hover:text-gray-200">Sign Up</Button>
       </Link>
       <Link href={"/sign-in"}>
-        <Button>Sign In</Button>
+      <Button className="h-9 px-4 text-md bg-white text-[#0C1A3C] hover:bg-[#0A142A] hover:text-white rounded-full">Sign In</Button>
       </Link>
     </div>
   );
