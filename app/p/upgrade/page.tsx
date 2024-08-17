f75c150d62498d5661bd90fa1e58544fe4ac4075
DevilsKingX <118957648+AkhilTrivediX@users.noreply.github.com>
2024-08-17 21:34:51 +0530

    removed icon

--- app/p/upgrade/page.tsx
+++ app/p/upgrade/page.tsx
@@ -4,33 +4,7 @@ import { paidFeatures } from "@/lib/constants";
 import { Button } from "@/components/ui/button";
 
 export default function Upgrade({ params }: { params: { test: string } }) {
-    let fontSize = '56px'
-
-    let iconSet = [
-            <Book size={fontSize} className="upgradeBGIcon"/>,
-            <Album size={fontSize} className="upgradeBGIcon"/>,
-            <BookA size={fontSize} className="upgradeBGIcon"/>,
-            <BookOpen size={fontSize} className="upgradeBGIcon"/>,
-            <Notebook size={fontSize} className="upgradeBGIcon"/>,
-            <LibraryBig size={fontSize} className="upgradeBGIcon"/>,
-            <BookImage size={fontSize} className="upgradeBGIcon"/>,
-            <NotebookPen size={fontSize} className="upgradeBGIcon"/>,
-            <FileHeart size={fontSize} className="upgradeBGIcon"/>,
-            <Brain size={fontSize} className="upgradeBGIcon"/>,
-            <Bot size={fontSize} className="upgradeBGIcon"/>,
-            <Cloud size={fontSize} className="upgradeBGIcon"/>,
-            <Archive size={fontSize} className="upgradeBGIcon"/>,
-            <BrainCog size={fontSize} className="upgradeBGIcon"/>,
-            <FileQuestion size={fontSize} className="upgradeBGIcon"/>,
-            <GraduationCap size={fontSize} className="upgradeBGIcon"/>,
-            <BookText size={fontSize} className="upgradeBGIcon"/>,
-            <Shapes size={fontSize} className="upgradeBGIcon"/>,
-    ]
-
-
-
-
-    let bgIcons = Array(8).fill(iconSet).flat()
+    
     return (
       <div className="relative">
         <div className="flex flex-wrap text-fire justify-center items-center p-4 w-screen h-screen absolute top-0 left-0">
@@ -42,7 +16,7 @@ export default function Upgrade({ params }: { params: { test: string } }) {
             <div>Upgrade to Pro to enjoy unlimited generations and no limits.</div>
             <div className="w-full h-[1px] bg-fire my-2"></div>
             <div className="flex flex-col ">
-                <div className="flex gap-2">
+                <div className="flex gap-2" key={'heading'}>
                         <div className="flex grow items-center gap-1">Features</div>
                         <div className="w-[1px] h-full bg-fire"></div>
                         <div className="w-20 p-2 text-center bg-gray-200 rounded-lg">Free</div>
